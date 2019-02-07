99d7d6c09bed1e5f1ce8795f357a638a5373dd34
PauIGB <56a06@mail.ru>
2019-02-07 22:00:54 +0300

    final hw7 w/redux-action + newest libriaries

--- karpov_p(lesson7)_red_act/webpack.config.js
+++ karpov_p(lesson7)_red_act/webpack.config.js
@@ -1,84 +1,87 @@
 const path = require('path');
-
-const HtmlPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtactPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 const CleanWebpackPlugin = require('clean-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = (env, options) => {
-  const development = options.mode === 'development';
-  return {    
-  entry: {
-    main: path.resolve(__dirname, 'src', 'index.jsx'),
-  },
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: development ? 'bundle.js' : 'bundle.min.js',
-  },
-  resolve: {
-    extensions: ['.js', '.jsx', '.json', ],
-    alias: {
-      components: path.resolve(__dirname, 'src', 'components'),
-      containers: path.resolve(__dirname, 'src', 'containers'),
-    },
-  },
-  devServer: {
-    contentBase: path.join(__dirname, 'src'),
-    overlay: true,
-    port: 9000,
-    stats: 'errors-only',  
-    compress: true, 
-    historyApiFallback: true,
-  },
-  devtool: development ? 'eval-source-map' : false,
-  module: {
-    rules: [
-      { 
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
+const development = options.mode === 'development';
+    return {
+        entry: {
+            main: path.resolve(__dirname, 'src', 'index.jsx'),
         },
-      },
-      { test: /\.(sa|sc|c)ss$/,
-        use: [ 'style-loader', MiniCssExtractPlugin.loader, 'css-loader', 
-          {
-            loader: 'postcss-loader',
-            options: {
-              ident: 'postcss',            
-              plugins: (loader) => [                     
-                development ? (require('autoprefixer')(), require('postcss-preset-env'))  : require('cssnano')(), 
-                require('autoprefixer')(),
-                require('postcss-preset-env')                                          
-              ]                                             
-            }
-          }, 'sass-loader']
-      },
-      {
-        test: /\.(png|gif|jpe?g)$/,
-        use: [
-            {
-              loader: 'file-loader',
-              options: {
-                  name: 'img/[name].[ext]',
-              },
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: development ? 'bundle.js' : 'bundle.min.js',
+        },
+        resolve: {
+            extensions: ['.js', '.jsx', '.json', ],
+            alias: {
+              components: path.resolve(__dirname, 'src', 'components'),
+              containers: path.resolve(__dirname, 'src', 'containers'),
+              reducers: path.resolve(__dirname, 'src', 'reducers'),
+              actions: path.resolve(__dirname, 'src', 'actions'),
+              middleware: path.resolve(__dirname, 'src', 'middleware'),
             },
-          ]
-      }     
-    ]
-  },
-  plugins: [    
-    new HtmlPlugin({
-      template: path.resolve(__dirname, 'src', 'index.html'),
-      filename: 'index.html',
-    }),
-    new MiniCssExtractPlugin({
-      filename: development ? 'bundle.css' : 'bundle.min.css'
-    }),
-    new CopyWebpackPlugin([
-      {from:'src/favicon.ico',to:'./'},
-    ]), 
-    new CleanWebpackPlugin('dist', {})  
-  ]
-  }
+        },
+        devServer: {
+            contentBase: path.join(__dirname, 'src'),
+            overlay: true,
+            stats: 'errors-only',
+            compress: true,
+            historyApiFallback: true,           
+        },     
+        devtool: development ? 'eval-source-map' : false,
+        module: {
+            rules: [
+                {
+                    test: /\.jsx?$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: 'babel-loader',
+                    },
+                },
+                {
+                    test: /\.(sa|sc|c)ss$/,
+                    use: ['style-loader', MiniCssExtactPlugin.loader, 'css-loader',
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            ident: 'postcss',
+                            plugins: (loader) => [
+                             development ? 
+                                (require('autoprefixer')(), require('postcss-preset-env'))
+                                :
+                                require('cssnano')(), require('autoprefixer')(), require('postcss-preset-env'),
+                            ],
+                        },
+                    }, 'sass-loader'],
+                },
+                {
+                    test: /\.(png|gif|jpe?g)$/,
+                    use: [
+                        {
+                          loader: 'file-loader',
+                          options: {
+                              name: 'img/[name].[ext]',
+                          },
+                        },
+                    ]
+                },     
+            ]
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: path.resolve(__dirname, 'src', 'index.html'),
+                filename: 'index.html',
+            }),
+            new MiniCssExtactPlugin({
+                filename: development ? 'bundle.css' : 'bundle.min.css',
+            }),
+            new CopyWebpackPlugin([
+                {from:'src/favicon.ico',to:'./'},
+            ]), 
+            new CleanWebpackPlugin('dist', {}),
+        ],    
+    };
 };
